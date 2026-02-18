11dcb12fc716ef849ddc2162439ba1170dd4f0bf
hassanhrz94-ctrl <hassanhrz94@gmail.com>
2026-02-18 19:48:42 +0600

    login js done

--- script/login.js
+++ script/login.js
@@ -1,1 +1,20 @@
-console.log("login functinatoy")
+document.getElementById('login-btn').addEventListener('click', function (){
+    const numberInput = document.getElementById('input-number');
+    const contactNumber = numberInput.value;
+    console.log(contactNumber)
+
+    const inputPin = document.getElementById('input-pin')
+    const pin = inputPin.value;
+    console.log(pin);
+    
+    if(contactNumber=="01234567890" && pin=="1234"){
+        alert('login success');
+        
+        window.location.assign('/home.html ')
+    }
+    
+    else{
+        alert('login failed');
+        return;
+    }
+})
