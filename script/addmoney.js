d22615b019ca78139fc424f78d18a0e49800349d
hassanhrz94-ctrl <hassanhrz94@gmail.com>
2026-02-19 15:30:31 +0600

    hidden

--- script/addmoney.js
+++ script/addmoney.js
@@ -1,3 +1,31 @@
  document.getElementById("add-money-btn").addEventListener('click',function(){
-    
+    // bank account get
+    const bankAccount = getValueFromInput("add-money-bank");
+    console.log(bankAccount)
+    if(bankAccount=== "select a Bank"){
+        alert('Please Select a bank')
+        return;
+    }
+    // get bank account number
+
+    const accno =getValueFromInput("add-money-number")
+    if(accno.length!= 11){
+        alert("Invalid acc NO")
+        return;
+    }
+
+    // get ammount
+    const amount = getValueFromInput("add-money-amount")
+    const currentBalance = getBalance()
+    const newBalance = currentBalance + Number(amount);
+   
+    const pin = getValueFromInput("add-money-pin")
+    if(pin=== "1234"){
+        alert(`Add Money Success from | ${bankAccount} at ${new Date()}`)
+        setBalance(newBalance);
+    }
+    else{
+        alert("Invalid pin")
+        return;
+    }
  })
--- script/machine.js
+++ script/machine.js
@@ -20,3 +20,18 @@ function setBalance (value){
     const balanceElement = document.getElementById('balance');
     balanceElement.innerText = value;
 }
+
+// machine id > hide all > show id 
+
+function showOnly(id){
+    
+    const addmoney = document.getElementById("add-money");
+    const cashout = document.getElementById("cashout");
+    
+    addmoney.classList.add("hidden")
+    cashout.classList.add("hidden")
+
+    const selected = document.getElementById(id);
+    selected.classList.remove("hidden");
+    
+}
