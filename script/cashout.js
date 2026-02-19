3ead17dcb005a919af312d611c4ccad5d148e015
hassanhrz94-ctrl <hassanhrz94@gmail.com>
2026-02-19 11:45:03 +0600

    with draw money

---
+++ script/cashout.js
@@ -0,0 +1,42 @@
+document.getElementById('cashout-btn').addEventListener('click',function(){
+    // // get the agent number & validate
+ const cashoutNumberInput =   document.getElementById('cashout-number')
+ const cashoutNumber = cashoutNumberInput.value;
+ console.log(cashoutNumber)
+// get the amount
+ const cashoutAmountInput = document.getElementById('cashout-Amount')
+ const cashoutAmount = cashoutAmountInput.value;
+ console.log(cashoutAmount);
+ if(cashoutAmount.length !=11){
+    alert('Invalid Agent Number')
+    return;
+
+ }
+// get the current balance
+
+ const balanceElement = document.getElementById('balance')
+ const balance = balanceElement.innerText
+ console.log(balance);
+
+//  calculate the new balance
+ const newBalance = Number(balance) - Number(cashoutAmount);
+ 
+ if(newBalance< 0 ){
+    alert('invalid amount');
+    return
+ }
+// get the pin and verify
+
+ const cashoutPinInput = document.getElementById('cashout-pin')
+ const pin = cashoutPinInput.value;
+  if(pin==='1234'){
+    alert("cashout successfull")
+     console.log("new balance",newBalance);
+     balanceElement.innerText = newBalance;
+  }
+  else{
+    alert('Invalid Pin')
+    return;
+  }
+
+})
